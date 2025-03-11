ca4dc72c774969bc7fe61a27b732fe7b5e4e8fcb
tyrantcourt <schmidtc2001@gmail.com>
2025-03-11 10:23:16 -0500

    Added Interactivity on Page
    Added functionality to play RPS with buttons
    Relevant text is no longer in-console!

--- rps.js
+++ rps.js
@@ -1,14 +1,17 @@
 playGame();
 
-function playGame() {
+async function playGame() {
     // Initializes scores
     let humanScore = 0;
     let computerScore = 0;
+
     // Plays a number of rounds, logs score count at end of each
-    // [BROKEN! NEVER INCREMENTS computerScore !!]
     let i = 0;
     while (i < 5) {
-        roundResult = playRound();
+        // Wait for user input
+        let humanChoice = await getHumanChoice();
+
+        let roundResult = playRound(humanChoice);
         if (roundResult === "win") {
             humanScore++;
         }
@@ -16,18 +19,22 @@ function playGame() {
             computerScore++;
         }
         i++;
-        console.log("Computer Points: " + computerScore + "\nHuman Points: " + humanScore);
+        alert("Computer Points: " + computerScore + "\nHuman Points: " + humanScore);
     }
     // Declares winner
+    const resultsDisplay = document.querySelector("#resultsDisplay");
+    const content = document.createElement("div");
+    content.classList.add("content");
     if (computerScore === humanScore) {
-        alert("Tie! Same amount of points.");
+        content.textContent = "Tie! Same amount of points.";
     }
     else if (computerScore > humanScore) {
-        alert("You lose! Less points.");
+        content.textContent = "Lose... you had less points.";
     }
     else {
-        alert("You win! You got more points!");
+        content.textContent = "Win! You had more points!";
     }
+    resultsDisplay.appendChild(content);
 }
 
 // Randomly generates rock, paper, or scissors
@@ -39,50 +46,66 @@ function getComputerChoice(rock, paper, scissors) {
 
 // Prompts user for rock, paper, or scissors
 function getHumanChoice(string) {
-    let choice = prompt("Rock, Paper, or Scissors?");
-    return choice.toLowerCase();
+    return new Promise(resolve => {
+        // Link to buttons in HTML page
+        const rockBtn = document.querySelector("#rockBtn");
+        const paperBtn = document.querySelector("#paperBtn");
+        const scissorsBtn = document.querySelector("#scissorsBtn");
+
+        // Adds event listeners to buttons
+        rockBtn.addEventListener("click", () => resolve("rock"));
+        paperBtn.addEventListener("click", () => resolve("paper"));
+        scissorsBtn.addEventListener("click", () => resolve("scissors"));
+        });
 }
 
 // Runs one round of Rock Paper Scissors
-function playRound(humanChoice, computerChoice) {
-    // Uses chosen answer
-    let human = getHumanChoice();
-        
+function playRound(humanChoice) {
     // Uses randomly generated answer
     const rock = "rock";
     const paper = "paper";
     const scissors = "scissors";
     let computer = getComputerChoice(rock, paper, scissors);
     
-    // Game logic
-    if (human === computer) {
-        console.log("Tie! Same answer!");
-        roundResult = "tie";
-    }
-    else if (human === "rock" && computer === "paper") {
-        console.log("You lose! Paper beats rock!")
-        roundResult = "loss";
-    }
-    else if (human === "rock" && computer === "scissors") {
-        console.log("You win! Rock beats scissors!")
-        roundResult = "win";
-    }
-    else if (human === "paper" && computer === "scissors") {
-        console.log("You lose! Scissors beats paper!")
-        roundResult = "loss";
-    }
-    else if (human === "paper" && computer === "rock") {
-        console.log("You win! Paper beats rock!")
-        roundResult = "win";
-    }
-    else if (human === "scissors" && computer === "rock") {
-        console.log("You lose! Rock beats scissors!")
-        roundResult = "loss";
-    }
-    else if (human === "scissors" && computer === "paper") {
-        console.log("You win! Scissors beats paper!")
-        roundResult = "win";
-    }
-    return roundResult;
-}
+    // Helps send round result to HTML
+    const roundResult = document.querySelector("#roundResult");
+    const contentRound = document.createElement("div");
+    contentRound.classList.add("contentRound");
 
+    // Game logic
+    if (humanChoice === computer) {
+        contentRound.textContent = "Tie! Same answer!";
+        roundResult.appendChild(contentRound);
+        return "tie";
+    }
+    else if (humanChoice === "rock" && computer === "paper") {
+        contentRound.textContent = "You lose! Paper beats rock!";
+        roundResult.appendChild(contentRound);
+        return "loss";
+    }
+    else if (humanChoice === "rock" && computer === "scissors") {
+        contentRound.textContent = "You win! Rock beats scissors!";
+        roundResult.appendChild(contentRound);
+        return "win";
+    }
+    else if (humanChoice === "paper" && computer === "scissors") {
+        contentRound.textContent = "You lose! Scissors beats paper!";
+        roundResult.appendChild(contentRound);
+        return "loss";
+    }
+    else if (humanChoice === "paper" && computer === "rock") {
+        contentRound.textContent = "You win! Paper beats rock!";
+        roundResult.appendChild(contentRound);
+        return "win";
+    }
+    else if (humanChoice === "scissors" && computer === "rock") {
+        contentRound.textContent = "You lose! Rock beats scissors!";
+        roundResult.appendChild(contentRound);
+        return "loss";
+    }
+    else if (humanChoice === "scissors" && computer === "paper") {
+        contentRound.textContent = "You win! Scissors beats paper!";
+        roundResult.appendChild(contentRound);
+        return "win";
+    }
+}
